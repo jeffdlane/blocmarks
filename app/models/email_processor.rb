334d8cf54cb72630cdff0ef966ee7c8656965b15
vagrant <vagrant@rails-dev-box>
2014-01-28 20:00:31 +0000

    putting title_processor code in self.process

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,19 +1,26 @@
 class EmailProcessor
   def self.process(email)
-    title_processor(email.subject)
-    Bookmark.create!({ title: @title, url: @url, tag_list: email.body })
-  end
-
-  def title_processor(email_subject)
-    if email_subject.include? ","
-      email_subject = email_subject.split(",").strip
+    if email.subject.include? ","
+      email_subject = email.subject.split(",").strip
       @title = email_subject.first
       @url = email_subject.last
     else
       @title = email_subject
       @url = email_subject
     end
+    Bookmark.create!({ title: @title, url: @url, tag_list: email.body })
   end
+
+  # def title_processor(email_subject)
+  #   if email_subject.include? ","
+  #     email_subject = email_subject.split(",").strip
+  #     @title = email_subject.first
+  #     @url = email_subject.last
+  #   else
+  #     @title = email_subject
+  #     @url = email_subject
+  #   end
+  # end
 
 
 end
