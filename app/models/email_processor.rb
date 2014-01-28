bcac93689f3fb31ef35d4f4c44da5e4db6c417a0
vagrant <vagrant@rails-dev-box>
2014-01-28 18:01:15 +0000

    add tags processing to email

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,5 +1,11 @@
 class EmailProcessor
   def self.process(email)
-    Bookmark.create!({ title: email.subject, url: email.body })
+    Bookmark.create!({ title: email.subject, url: email.body, tag_list: email.body })
   end
+
+  def self.tags_processor(email)
+    @tags = email.body.split(", ")
+  end
+
 end
+
