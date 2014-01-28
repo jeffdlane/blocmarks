67d5826eb60aeaae272f26a7438738787f05de9e
vagrant <vagrant@rails-dev-box>
2014-01-28 21:28:02 +0000

    fixed else clause in EmailProcessor

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -5,21 +5,23 @@ class EmailProcessor
       @title = email_subject.first
       @url = email_subject.last
     else
-      @title = email_subject
-      @url = email_subject
+      @title = email.subject
+      @url = email.subject
     end
     Bookmark.create!({ title: @title, url: @url, tag_list: email.body })
   end
 
   # def title_processor(email_subject)
-  #   if email_subject.include? ","
-  #     email_subject = email_subject.split(",").strip
-  #     @title = email_subject.first
-  #     @url = email_subject.last
-  #   else
-  #     @title = email_subject
-  #     @url = email_subject
-  #   end
+    # if email.subject.include? ","
+    #   email_subject = email.subject.split(",").collect {|s| s.strip}
+    #   @title = email_subject.first
+    #   @url = email_subject.last
+    # else
+    #   @title = email.subject
+    #   @url = email.subject
+    # end
+    # @title
+    # @url
   # end
 
 
