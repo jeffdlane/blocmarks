cf5273843207b636263432324fb09c948d975276
vagrant <vagrant@rails-dev-box>
2014-02-11 03:34:26 +0000

    added email authentication

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,5 +1,14 @@
 class EmailProcessor
   def self.process(email)
+    if self.authenticate(email)
+      self.create_bookmark(email)
+  end
+
+  def self.authenticate(email)
+    User.find_by_email(email.from).present?
+  end
+
+  def self.create_bookmark(email)   
     if email.subject.include? ","
       email_subject = email.subject.split(",").collect {|s| s.strip}
       @title = email_subject.first
@@ -8,22 +17,9 @@ class EmailProcessor
       @title = email.subject
       @url = email.subject
     end
-    Bookmark.create!({ title: @title, url: @url, tag_list: email.body })
+
+    Bookmark.create!({ title: @title, url: @url, tag_list: email.body })    
   end
-
-  # def title_processor(email_subject)
-    # if email.subject.include? ","
-    #   email_subject = email.subject.split(",").collect {|s| s.strip}
-    #   @title = email_subject.first
-    #   @url = email_subject.last
-    # else
-    #   @title = email.subject
-    #   @url = email.subject
-    # end
-    # @title
-    # @url
-  # end
-
 
 end
 
